cc43ea68333d3732d6b6aa25aa48ed4b55c79582
ShowTimeFFS <daniel@cherry.tv>
2021-05-31 17:21:23 +0300

    1

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ function openNav(event) {
     document.getElementById("main").style.backgroundColor = "rgba(0,0,0,0.6)";
     document.getElementById("main-2").style.backgroundColor = "rgba(0,0,0,0.6)";
 }
+
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.backgroundColor = "unset";
@@ -11,6 +12,21 @@ function closeNav() {
 }
 
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+
+function gtag() {
+    dataLayer.push(arguments);
+}
+
 gtag('js', new Date());
 gtag('config', 'G-S0ZDB4CRH4');
+
+function init() {
+    var vidDefer = document.getElementsByTagName('iframe');
+    for (var i = 0; i < vidDefer.length; i++) {
+        if (vidDefer[i].getAttribute('data-src')) {
+            vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+        }
+    }
+}
+
+window.onload = init;
